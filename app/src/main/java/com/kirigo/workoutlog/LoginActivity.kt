1a85edcb4d0e15e818494d1e70d8c5cba3fc96ad
mercyndungukirigo@gmail.com <mercyndungukirigo@gmail.com>
2022-06-04 15:11:36 +0300

    first commit

---
+++ app/src/main/java/com/kirigo/workoutlog/LoginActivity.kt
@@ -0,0 +1,51 @@
+package com.kirigo.workoutlog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputLayout
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var tilEmail: TextInputLayout
+    lateinit var etEmail: EditText
+    lateinit var tilPassword: TextInputLayout
+    lateinit var etPassword: EditText
+    lateinit var btnLogin: Button
+    lateinit var tvSignup: TextView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        btnLogin = findViewById(R.id.btnLogin)
+        tilEmail = findViewById(R.id.tilEmail)
+        etEmail = findViewById(R.id.etEmail)
+        tilPassword = findViewById(R.id.tilPassword)
+        tvSignup = findViewById(R.id.tvSignup)
+        etPassword = findViewById(R.id.etPassword)
+
+
+        tvSignup.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+        btnLogin.setOnClickListener {
+            validation()
+        }
+
+    }
+    fun validation(){
+        var email = etEmail.text.toString()
+        var password = etPassword.text.toString()
+        if(email.isBlank()){
+            tilEmail.error = "Email is required"
+        }
+        if(password.isBlank()){
+            tilPassword.error = "Password is required"
+        }
+    }
+}
---
+++ app/src/main/java/com/kirigo/workoutlog/SignUpActivity.kt
@@ -0,0 +1,64 @@
+package com.kirigo.workoutlog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputLayout
+
+class SignUpActivity : AppCompatActivity() {
+    lateinit var etEnterPassword: EditText
+    lateinit var etConfirmPassword: EditText
+    lateinit var btnSignUp: Button
+    lateinit var tvLogin2: TextView
+    lateinit var tilConfirmPassword: TextInputLayout
+
+
+//    tvLogin2.setOnClickListener {
+//        val intent = Intent(this, LoginActivity::class.java)
+//        startActivity(intent)
+//    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    btnSignUp = findViewById(R.id.btnSignUp)
+    etConfirmPassword = findViewById(R.id.etConfirmPassword)
+    tvLogin2 = findViewById(R.id.tvLogin2)
+    etEnterPassword = findViewById(R.id.etEnterPassword)
+    tilConfirmPassword = findViewById(R.id.tilConfirmPassword)
+
+
+    tvLogin2.setOnClickListener {
+       val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+   }
+    btnSignUp.setOnClickListener {
+        val intent = Intent(this, MainPage::class.java)
+        startActivity(intent)
+    }
+    btnSignUp.setOnClickListener {
+        validity()
+    }
+    }
+    fun validity(){
+        var password1 = etEnterPassword.text.toString()
+        var password2 = etConfirmPassword.text.toString()
+        if(password1!=password2){
+            tilConfirmPassword.error = "Password does not match!"
+        }
+
+//        fun validation(){
+//            var email = etEmail.text.toString()
+//            var password = etPassword.text.toString()
+//            if(email.isBlank()){
+//                tilEmail.error = "Email is required"
+//            }
+//            if(password.isBlank()){
+//                tilPassword.error = "Password is required"
+//            }
+//        }
+//    }
+    }
+}
