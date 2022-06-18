5074eaebcd2459bb169a95e4546935c125f55333
mercyndungukirigo@gmail.com <mercyndungukirigo@gmail.com>
2022-06-18 09:00:54 +0300

    Added fragments and implemented them

--- app/src/main/java/com/kirigo/workoutlog/HomeActivity.kt
+++ app/src/main/java/com/kirigo/workoutlog/HomeActivity.kt
@@ -2,10 +2,46 @@ package com.kirigo.workoutlog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var fcvHome: FragmentContainerView
+    lateinit var bnv: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home2)
+        castviews()
+        setupBottomNav()
+    }
+
+    fun castviews() {
+        fcvHome = findViewById(R.id.fcvHome)
+        bnv = findViewById(R.id.bnvBottom)
+    }
+
+    fun setupBottomNav() {
+        bnv.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.plan -> {
+                    val transaction = supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.fcvHome, PlanFragment())
+                    transaction.commit()
+                    true
+                }
+                R.id.track -> {
+                    val transaction = supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.fcvHome, TrackFragment())
+                    transaction.commit()
+                    true
+                }
+                R.id.profile -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fcvHome, ProfileFragment()).commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
