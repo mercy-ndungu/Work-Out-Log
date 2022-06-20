a85aa191d03cb18ff1ce5b9d52bcd6c535f6a2e1
mercyndungukirigo@gmail.com <mercyndungukirigo@gmail.com>
2022-06-20 07:50:24 +0300

    Added view binding

--- app/src/main/java/com/kirigo/workoutlog/HomeActivity.kt
+++ app/src/main/java/com/kirigo/workoutlog/HomeActivity.kt
@@ -4,25 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentContainerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.kirigo.workoutlog.databinding.ActivityHome2Binding
+import com.kirigo.workoutlog.databinding.ActivityHomeBinding
+import com.kirigo.workoutlog.databinding.ActivitySignUpBinding
 
 class HomeActivity : AppCompatActivity() {
-    lateinit var fcvHome: FragmentContainerView
-    lateinit var bnv: BottomNavigationView
+    lateinit var binding: ActivityHome2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home2)
-        castviews()
+//        setContentView(R.layout.activity_home2)
+        binding = ActivityHome2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
         setupBottomNav()
-
     }
-
-    fun castviews() {
-        fcvHome = findViewById(R.id.fcvHome)
-        bnv = findViewById(R.id.bnvBottom)
-    }
-
     fun setupBottomNav() {
-        bnv.setOnItemSelectedListener { item ->
+        binding.bnvBottom.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.plan -> {
                     val transaction = supportFragmentManager.beginTransaction()
